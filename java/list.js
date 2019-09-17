9808d73ad8f9358d89f50aed4447b3359552771e
Chantakrak Ath-Ly <chantakrak.ath-ly@centre.edu>
2019-09-17 15:54:59 -0400

    stuff

---
+++ java/list.js
@@ -0,0 +1,23 @@
+var pathOne = function()
+{
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "Minecraft";
+        node.setAttribute("onclick","pathTwo()");
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+
+    document.querySelector("#button1")
+        .setAttribute("onclick", "pathTwo()");
+}
+
+var pathTwo = function(){
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "ForkKnife";
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+
+}
