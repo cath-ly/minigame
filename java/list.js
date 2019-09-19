ab82fa94f168aa1b91103dab1e213251725e4994
maxsteele2314 <54860529+maxsteele2314@users.noreply.github.com>
2019-09-18 20:39:05 -0400

    Update list.js

--- java/list.js
+++ java/list.js
@@ -2,7 +2,7 @@ var pathOne = function()
 {
     var node =
         document.createElement("h2");
-        node.innerHTML = "Minecraft";
+        node.innerHTML = "Minecraft: Continue but at the risk of losing everything stop when you've had enough... ;)";
         node.setAttribute("onclick","pathTwo()");
     var body =
         document.querySelector("body");
@@ -15,7 +15,7 @@ var pathOne = function()
 var pathTwo = function(){
     var node =
         document.createElement("h2");
-        node.innerHTML = "ForkKnife";
+        node.innerHTML = "Enter cave with no food";
     var body =
         document.querySelector("body");
         body.appendChild(node);
@@ -24,7 +24,43 @@ var pathTwo = function(){
 var pathThree = function(){
     var node =
         document.createElement("h2");
-        node.innerHTML = "CREEPER";
+        node.innerHTML = "Find big Iron deposit";
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+}
+
+var pathFour = function(){
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "Continue in cave with no torches";
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+}
+
+var pathFive = function(){
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "Mining Dimonds";
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+}
+
+var pathSix = function(){
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "Headed back home and CREEPER!";
+    var body =
+        document.querySelector("body");
+        body.appendChild(node);
+}
+
+var pathSeven = function(){
+    var node =
+        document.createElement("h2");
+        node.innerHTML = "You Died!";
     var body =
         document.querySelector("body");
         body.appendChild(node);
