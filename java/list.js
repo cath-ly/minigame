ddb5cbdc14bbcac868af282399e5b028e181c1b6
maxsteele2314 <54860529+maxsteele2314@users.noreply.github.com>
2019-09-18 20:53:43 -0400

    update

--- java/list.js
+++ java/list.js
@@ -3,13 +3,10 @@ var pathOne = function()
     var node =
         document.createElement("h2");
         node.innerHTML = "Minecraft: Continue but at the risk of losing everything stop when you've had enough... ;)";
-        node.setAttribute("onclick","pathTwo()");
     var body =
         document.querySelector("body");
         body.appendChild(node);
 
-    document.querySelector("#button1")
-        .setAttribute("onclick", "pathThree()");
 }
 
 var pathTwo = function(){
